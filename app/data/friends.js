bb53766eb427f6ec2a6455668aa1db46a9f53733
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-07-28 17:11:17 -0700

    added dummy data for friends array

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsArr = [
+    {
+        userName: "Fred Rogers",
+        userPhoto: "https://en.wikipedia.org/wiki/Fred_Rogers#/media/File:Fred_Rogers,_late_1960s.jpg",
+        userScores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    }
+];
+
+module.exports = friendsArr;
