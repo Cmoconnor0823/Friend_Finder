cd0b68972dbb5419d2f378e9cf71619469b34e5d
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-07-30 15:20:17 -0700

    added second friend for testing

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,14 @@
 var friendsArr = [
     {
         userName: "Fred Rogers",
-        userPhoto: "https://en.wikipedia.org/wiki/Fred_Rogers#/media/File:Fred_Rogers,_late_1960s.jpg",
-        userScores: [
-            5,
-            5,
-            5,
-            5,
-            5,
-            1,
-            1,
-            1,
-            1,
-            1
-        ]
+        userPhoto: "https://upload.wikimedia.org/wikipedia/commons/3/36/Fred_Rogers%2C_late_1960s.jpg",
+        userScores: [5, 5, 5, 5, 5, 1, 1, 1, 1, 1]
+    },
+    {
+        userName: "LeVar Burton",
+        userPhoto: "https://upload.wikimedia.org/wikipedia/commons/1/15/LeVar_Burton.png",
+        userScores: [5, 4, 3, 2, 5, 1, 2, 3, 2, 1]
+
     }
 ];
 
