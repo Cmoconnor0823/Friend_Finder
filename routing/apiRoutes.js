5b2b27416fa81984de0a8ebcefcd0b4ee39fb164
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-07-30 15:01:08 -0700

    finished attempted logic for comparing scores

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -2,11 +2,46 @@
 var friendsArr = require("../data/friends");
 
 // Begin the routing here
-module.exports = function(app) {
-// now the get request for all friend data
-app.get("/api/friends", function(req, res) {
-    res.json(friendsArr);
-});
+module.exports = function (app) {
+    // now the get request for all friend data
+    app.get("/api/friends", function (req, res) {
+        res.json(friendsArr);
+    });
 
+    //Api Post request
+    //This code will take in the survey data and compare it to the 
+    //data in the friends array
+    app.post("/api/friends", function (req, res) {
+        friendsArr.push(req.body);
+        //var grabbing current user's score
+        var friendScore = req.body.scores;
+        //closest matching frined will have a score closest to 0
+        var matchFriend = 0;
+        //this sets the worst possible score
+        var lowestDiff = 50;
 
+        //Now call in the data from the Friends array and run the score 
+        // through each friend and then compare scores
+        for (var i = 0; i < (friendsArr.length - 1); i++) {
+            //create a var to compare score
+            var compareScore = friendsArr[i].scores;
+            var totalDiff = 0;
+
+            //Now compare the score for each friend in the data
+            for (var j = 0; j < friendScore.length; j++) {
+                //And now do the math to compare each question's score between the friends
+                // use absolute value of current score - the absolute value of compareScore
+                var eachDiff = Math.abs(parseInt(friendScore[j]) - parseInt(compareScore[j]));
+                //now add the total of each diff to the total diff to get the final friend score
+                totalDiff = totalDiff + eachDiff;
+            }
+            // and set a stop/ best friend 
+            if (totalDiff < lowestDiff) {
+                lowestDiff = totalDiff;
+                matchFriend = [i];
+            }
+        }
+
+        res.send(friendsArr[matchFriend]);
+    });
 };
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var path = require("path");
 var app = express();
 
 // this sets the initial port.. should I be setting this or heroku?
+//answered using process.env.PORT allows heroku to choose and || says or use this port
 var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing--taken from in class assingment week 13 day 3 act 16  
