07871deaee620586b1a57eb638378b23c08caedd
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-07-30 15:46:47 -0700

    fixed __dirname from 1 _ to 2 __

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -3,15 +3,21 @@ var path = require("path");
 // Begin the routing here
 module.exports = function(app) {
 // below code for each possible route for the html pages
+app.get("/home", function(req, res) {
+    res.sendfile(path.join(__dirname, "../public/home.html"));
+});
+
 app.get("/survey", function(req, res) {
-    res.sendfile(path.join(_dirname, "../public/survey.html"));
+    res.sendfile(path.join(__dirname, "../public/survey.html"));
 });
 
 //default route request to home page
 app.get("*", function(req, res) {
-    res.sendfile(path.join(_dirname, "../public/home.html"));
+    res.sendfile(path.join(__dirname, "../public/home.html"));
 });
 
-
+app.use(function (req, res) {
+    res.sendFile(path.join(__dirname, '/../public/home.html'));
+  });
 
 };
