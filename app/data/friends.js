40fa1d9b575532b45130ce1f45d6c603af597b40
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-07-31 14:21:18 -0700

    fixed express deprecated error

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,14 +2,20 @@ var friendsArr = [
     {
         userName: "Fred Rogers",
         userPhoto: "https://upload.wikimedia.org/wikipedia/commons/3/36/Fred_Rogers%2C_late_1960s.jpg",
-        userScores: [5, 5, 5, 5, 5, 1, 1, 1, 1, 1]
+        userScores: [1, 4, 1, 5, 4, 1, 1, 2, 3, 1]
     },
     {
         userName: "LeVar Burton",
         userPhoto: "https://upload.wikimedia.org/wikipedia/commons/1/15/LeVar_Burton.png",
-        userScores: [5, 4, 3, 2, 5, 1, 2, 3, 2, 1]
+        userScores: [4, 4, 3, 2, 2, 1, 3, 1, 2, 3]
 
+    },
+    {
+        userName: "Bob Ross",
+        userPhoto: "https://upload.wikimedia.org/wikipedia/commons/1/15/LeVar_Burton.png",
+        userScores: [5, 4, 3, 2, 5, 1, 2, 3, 2, 1]
     }
+
 ];
 
 module.exports = friendsArr;
--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,23 +1,37 @@
+var express = require('express');
+var app = express();
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+});
+
+
+
 var path = require("path");
 
 // Begin the routing here
-module.exports = function(app) {
-// below code for each possible route for the html pages
-app.get("/home", function(req, res) {
-    res.sendfile(path.join(__dirname, "../public/home.html"));
-});
+module.exports = function (app) {
+    // below code for each possible route for the html pages
+    app.get("/home", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
 
-app.get("/survey", function(req, res) {
-    res.sendfile(path.join(__dirname, "../public/survey.html"));
-});
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
 
-//default route request to home page
-app.get("*", function(req, res) {
-    res.sendfile(path.join(__dirname, "../public/home.html"));
-});
+    //default route request to home page
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
 
-app.use(function (req, res) {
-    res.sendFile(path.join(__dirname, '/../public/home.html'));
-  });
 
 };
